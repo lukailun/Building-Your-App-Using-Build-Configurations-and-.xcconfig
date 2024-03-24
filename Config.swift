e47ab391376b47c509e6d09fc43ea5e1ad25ffcb
Karen <957458041@qq.com>
2024-03-24 20:44:49 +0800

    update

--- Config.swift
+++ Config.swift
@@ -30,6 +30,8 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import Foundation
+
 enum Config {
     static func stringValue(forKey key: String) -> String {
         guard let value = Bundle.main.object(forInfoDictionaryKey: key) as? String
